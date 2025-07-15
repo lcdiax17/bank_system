c304b66d0bef6aa1d2f462ab8f82dc310dd614d8
Lcdiax <diasdevs17@gmail.com>
2025-07-14 22:05:22 -0300

    Sistema acessando as opções corretamente agora, extrato com opção de selecionar o tipo de extrato criado, sistema testado com a opção de deposito apenas

--- src/system.py
+++ src/system.py
@@ -1,36 +1,36 @@
-'Sistema bancário básico'
+print('Sistema bancário')
 
-menu_de_operacao = [
-    0 Depositar
-    1 Sacar
-    2 Extrato Bancário
-    3 Sair
-]
+menu_de_operacao = """
+    [0] Depositar
+    [1] Sacar
+    [2] Extrato Bancário
+    [3] Sair
+=>  """
 
 
 LIMITE_DE_SAQUES_DIARIO = 3
 numero_de_saques = 0
 limite_por_saque = 500
 saldo = 0
-extrato = ""
+extrato = []
 
 
 while True:
 
     opcao_selecionada = input(menu_de_operacao)
 
-    if opcao_selecionada == 0:
+    if opcao_selecionada == "0":
 
         valor = float(input("Digite o valor que deseja depositar: "))
 
         if valor > 0:
             saldo += valor
-            extrato += f"Depósito: R$ {valor:.2f}"
+            extrato.append(f"Deposito: R$ {valor:.2f}")
 
         else:
             print("Erro! Valor depositado é invalído ou insuficiente")
 
-    elif opcao_selecionada == 1:
+    elif opcao_selecionada == "1":
 
         valor = float(input("Digite o valor que deseja sacar: "))
 
@@ -39,7 +39,8 @@ while True:
         saques_excedidos = numero_de_saques > LIMITE_DE_SAQUES_DIARIO
 
         if limite_de_saque_excedido:
-            print("Operação invalida!, limite de saque indisponivel para sua categoria")
+            print(
+                "Operação invalida!, limite de saque indisponivel para sua categoria")
 
         elif valor_do_saldo_excedido:
             print("Operação invalida! Saldo insuficiente")
@@ -48,21 +49,24 @@ while True:
             print("Operação invalida! Número de saques do dia atingiu seu limite!")
 
         elif valor > 0:
-            saldo - + valor
+            saldo -= valor
             numero_de_saques += 1
-            extrato += f"Saque: R$ {valor:.2f}"
+            extrato.append(f"Saque: R$ {valor:.2f}")
 
-    elif opcao_selecionada == 2:
+    elif opcao_selecionada == "2":
 
-        for tipo in extrato:
-            if tipo == "Deposito: " in extrato:
-                print(f"Extrato de depositos: \n {tipo}")
-            elif tipo == "Saque: " in extrato:
-                print(f"Extrato de saques: \n {tipo}")
-            else:
-                print(f"Extrato de saques: {extrato}")
+        tipo = input(
+            "Selecione o tipo de extrato:\n[1] - Deposito\n[2] - Saque\n[3] - Todos \n")
 
-    elif opcao_selecionada == 3:
+        if not extrato:
+            print("Nenhuma movimentação realizada")
+        else:
+            if tipo == "1":
+                for tipo_extrato in extrato:
+                    if "Deposito" in tipo_extrato:
+                        print(tipo_extrato)
+
+    elif opcao_selecionada == "3":
         break
 
     else:
